55603fdad0f3fd298fe188a17f5bbdaa04c82710
kotireddy10 <kotireddykummita10@gmail.com>
2025-07-19 09:11:46 +0530

    js

---
+++ knapsack.js
@@ -0,0 +1,18 @@
+const knapsack = (weights, values, W) => {
+  const n = weights.length;
+  const dp = Array(n + 1).fill().map(() => Array(W + 1).fill(0));
+
+  for (let i = 1; i <= n; i++) {
+    for (let w = 1; w <= W; w++) {
+      if (weights[i - 1] <= w) {
+        dp[i][w] = Math.max(dp[i - 1][w], values[i - 1] + dp[i - 1][w - weights[i - 1]]);
+      } else {
+        dp[i][w] = dp[i - 1][w];
+      }
+    }
+  }
+
+  return dp[n][W];
+};
+
+console.log(knapsack([2, 3, 4], [4, 5, 6], 5)); // Output: 9
